7724740b10ef5f4a1e4d2fdaac6d8bb9b053132e
kingkk <917536172@qq.com>
2018-11-22 19:01:21 +0800

    FirstSample

---
+++ Fundamentals/FirstSample/Constants2.java
@@ -0,0 +1,13 @@
+public class Constants2
+{
+	public static final double CM_PER_INCH = 2.54;
+	
+	public static void main(String args[])
+	{
+		double paperWidth = 8.5;
+		double paperHeight = 11;
+//		 qqqqqqqqqqqqqqqqqqCM_PER_INCH = 5;
+		System.out.println("Paper size in centimeer: "
+		+ paperWidth*CM_PER_INCH + " by " + paperHeight*CM_PER_INCH);
+	}
+}
